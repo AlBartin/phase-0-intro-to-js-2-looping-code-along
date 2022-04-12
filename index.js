6bfb15e862bd9afde4013ca0a9a3977966a1103f
alan <alan.martnz@gmail.com>
2022-04-12 14:56:03 -0700

    commit tree 556be7ec5a07b0b16673c9562d8de22b74db0999

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+//Code your solutions in this file
+function writeCards(names, event) {
+    const recipients =[];
+    for (let i = 0; i < names.length; i++) {
+      recipients.push(names[i]);
+      recipients[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+    }
+    return recipients;
+  }
+
+function countDown(integer) {
+    while(integer >= 0) {
+        console.log(integer);
+        integer--;
+    }
+  }
